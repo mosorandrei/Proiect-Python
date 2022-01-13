11f94a3b64ba8cdcfd1f63a52fbee5301ece6f3d
mosorandrei <mosorandrei49@gmail.com>
2022-01-13 18:39:44 +0200

    Commit requeste by our teacher: to provide multiple database functionality and do it for mysql

--- DB.py
+++ DB.py
@@ -1,4 +1,5 @@
 import cx_Oracle
+import mysql.connector
 
 dsn_tns = cx_Oracle.makedsn('localhost', '1521', service_name='XE')
 conn = cx_Oracle.connect(user=r'STUDENT', password='STUDENT', dsn=dsn_tns)
@@ -9,14 +10,14 @@ c.execute('DROP TABLE filedatabase')
 c = conn.cursor()
 c.execute('CREATE TABLE filedatabase('
           + 'file_id number(10) NOT NULL,'
-          + 'file_name varchar2(50) NOT NULL,'
-          + 'file_type varchar2(50) NOT NULL,'
-          + 'file_size varchar2(50) NOT NULL,'
-          + 'file_created varchar2(50) NOT NULL,'
-          + 'file_modified varchar2(50) NOT NULL,'
-          + 'file_accessed varchar2(50) NOT NULL,'
-          + 'public_key varchar2(50) NOT NULL,'
-          + 'product_prime varchar2(800) NOT NULL'
+          + 'file_name varchar2(255) NOT NULL,'
+          + 'file_type varchar2(255) NOT NULL,'
+          + 'file_size varchar2(255) NOT NULL,'
+          + 'file_created varchar2(255) NOT NULL,'
+          + 'file_modified varchar2(255) NOT NULL,'
+          + 'file_accessed varchar2(255) NOT NULL,'
+          + 'public_key varchar2(255) NOT NULL,'
+          + 'product_prime varchar2(255) NOT NULL'
           + ')')
 
 c = conn.cursor()
@@ -27,3 +28,31 @@ for row in c:
 
 c.close()
 conn.close()
+
+connection = mysql.connector.connect(host='localhost',
+                                     database='Mysql',
+                                     user='root',
+                                     password='STUDENT')
+
+cursor = connection.cursor()
+cursor.execute("Drop table filedatabase")
+
+cursor = connection.cursor()
+result = cursor.execute('CREATE TABLE filedatabase('
+                        + 'file_id int(10) NOT NULL,'
+                        + 'file_name varchar(255) NOT NULL,'
+                        + 'file_type varchar(255) NOT NULL,'
+                        + 'file_size varchar(255) NOT NULL,'
+                        + 'file_created varchar(255) NOT NULL,'
+                        + 'file_modified varchar(255) NOT NULL,'
+                        + 'file_accessed varchar(255) NOT NULL,'
+                        + 'public_key varchar(255) NOT NULL,'
+                        + 'product_prime varchar(255) NOT NULL'
+                        + ')')
+
+
+cursor = connection.cursor()
+result2 = cursor.execute("select * from filedatabase")
+
+for r in cursor:
+    print(r)
